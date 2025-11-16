45c992f90de09d5dcbeddd815a6c37c31c030db0
avirupThinkbridge <50024464+avirup2512@users.noreply.github.com>
2025-11-16 17:45:37 +0530

    Change the port in index

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ app.use((err, req, res, next) => {
 // -------------------------
 // START SERVER
 // -------------------------
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4200;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
